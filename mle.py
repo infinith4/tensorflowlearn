e121c0cc2a78fb387fe297f1cb9c9d8c4a55b1b0
infinith4@gmail.com <infinith4@gmail.com>
2016-10-31 22:59:47 +0900

    add training

--- mle.py
+++ mle.py
@@ -19,3 +19,39 @@ df1['t'] = 1
 
 df = pd.concat([df0,df1],ignore_index=True)
 train_set = df.reindex(permutation(df.index)).reset_index(drop=True)
+
+train_x = train_set[['x1','x2']].as_matrix()
+train_t = train_set['t'].as_matrix().reshape([len(train_set),1])
+x = tf.placeholder(tf.float32,[None,2])
+w = tf.Variable(tf.zeros([2,1]))
+w0 = tf.Variable(tf.zeros([1]))
+f = tf.matmul(x,w) + w0
+p = tf.sigmoid(f)
+
+t = tf.placeholder(tf.float32,[None,1])
+loss = -tf.reduce_sum(t*tf.log(p) + (1-t)+tf.log(1-p))
+train_step = tf.train.AdamOptimizer().minimize(loss)
+
+correct_prediction = tf.equal(tf.sign(p-0.5),tf.sign(t-0.5))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+i = 0
+for _ in range(0,20000):
+    i+=1
+    sess.run(train_step,feed_dict={x:train_x,t:train_t})
+    if i % 2000 == 0:
+        loss_val,acc_val = sess.run([loss,accuracy],feed_dict={x:train_x,t:train_t})
+        print('Step:%d,Loss:%f,Accuracy:%f' % (i,loss_val,acc_val))
+# Step:2000,Loss:7.463896,Accuracy:0.600000
+# Step:4000,Loss:5.210213,Accuracy:0.685714
+# Step:6000,Loss:4.460924,Accuracy:0.685714
+# Step:8000,Loss:4.315311,Accuracy:0.685714
+# Step:10000,Loss:4.309548,Accuracy:0.685714
+# Step:12000,Loss:4.309571,Accuracy:0.685714
+# Step:14000,Loss:4.309546,Accuracy:0.685714
+# Step:16000,Loss:4.309547,Accuracy:0.685714
+# Step:18000,Loss:4.309546,Accuracy:0.685714
+# Step:20000,Loss:4.309547,Accuracy:0.685714
